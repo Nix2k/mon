6f08c99502a66e194a2d5ea62fee5bdf380d6258
Nix2k <mihail.bokov@gmail.com>
2019-10-31 23:35:56 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,10 @@ Route::get('/', function () {
 });
 
 // Маршруты аутентификации...
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
+Route::get('/auth/login', 'Auth\AuthController@getLogin');
+Route::post('/auth/login', 'Auth\AuthController@postLogin');
+Route::get('/auth/logout', 'Auth\AuthController@getLogout');
 
 // Маршруты регистрации...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
+Route::get('/auth/register', 'Auth\AuthController@getRegister');
+Route::post('/auth/register', 'Auth\AuthController@postRegister');
